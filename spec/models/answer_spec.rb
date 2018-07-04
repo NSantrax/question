6b00ef6261f341d4a8e45f47acaf7e8602dbcf0e
NSantrax <ns2008p@mail.ru>
2018-07-04 19:06:28 +0300

    Fix without shoulda-matchers

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,14 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
   it "validate presence to body" do
-    expect(Answer.new(quest: Quest.new)).to_not be_valid
+    expect(Answer.new(quest: Quest.new(id: '11'))).to_not be_valid
   end
   it "validate presence to question" do
     expect(Answer.new(body: '123')).to_not be_valid
   end
-  
-   it "validate lenght of body" do
-    expect(Answer.new(body: '1')).to_not be_valid
+  it "validate lenght of body min" do
+    expect(Answer.new(body: '12', quest: Quest.new(id: '11'))).to_not be_valid
   end
+  
+  it "validate lenght of body max" do
+    expect(Answer.new(body: 'f'*5001, quest: Quest.new(id: '11'))).to_not be_valid
+  end
+  
   
 end
--- spec/models/quest_spec.rb
+++ spec/models/quest_spec.rb
@@ -1,14 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Quest, type: :model do
-  it "validate presence to title" do
+  it "validate presence to body" do
     expect(Quest.new(title: '123')).to_not be_valid
   end
-  it "validate presence to body" do
+  it "validate presence to title" do
     expect(Quest.new(body: '123')).to_not be_valid
   end
-   it "validate lenght of body" do
-    expect(Answer.new(body: '1')).to_not be_valid
+  
+   it "validate lenght of title min" do
+    expect(Quest.new(title: '13', body: '124r')).to_not be_valid
+  end
+  
+  it "validate lenght of title max" do
+    expect(Quest.new(title: 'f'*251, body: '123')).to_not be_valid
+  end
+  
+   it "validate lenght of body min" do
+    expect(Quest.new(title: '123', body: '12', )).to_not be_valid
+  end
+  
+  it "validate lenght of body max" do
+    expect(Quest.new(title: '123', body: 'f'*2001)).to_not be_valid
   end
   
    it "should have many answers" do
