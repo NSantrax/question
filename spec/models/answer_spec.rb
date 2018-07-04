914cfb08f8c28c35f0d8ff3b88d92aa7d054c3c9
NSantrax <ns2008p@mail.ru>
2018-07-04 22:01:03 +0300

    Add shoulda-matchers

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,19 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe Answer, type: :model do
-  it "validate presence to body" do
-    expect(Answer.new(quest: Quest.new(id: '11'))).to_not be_valid
+
+RSpec.describe Answer, :type => :model do
+  describe 'validations' do
+    it {should validate_presence_of :body}
+    it {should validate_presence_of :quest_id}
+    it {should validate_length_of(:body).is_at_least(3)} 
+    it {should validate_length_of(:body).is_at_most(5000)}
   end
-  it "validate presence to question" do
-    expect(Answer.new(body: '123')).to_not be_valid
+  describe 'associations' do
+    it { should belong_to(:quest) }
   end
-  it "validate lenght of body min" do
-    expect(Answer.new(body: '12', quest: Quest.new(id: '11'))).to_not be_valid
-  end
-  
-  it "validate lenght of body max" do
-    expect(Answer.new(body: 'f'*5001, quest: Quest.new(id: '11'))).to_not be_valid
-  end
-  
-  
 end
--- spec/models/quest_spec.rb
+++ spec/models/quest_spec.rb
@@ -1,34 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Quest, type: :model do
-  it "validate presence to body" do
-    expect(Quest.new(title: '123')).to_not be_valid
+  describe 'validations' do
+    it {should validate_presence_of :body}
+    it {should validate_presence_of :title}
+    it {should validate_length_of(:body).is_at_least(3)} 
+    it {should validate_length_of(:body).is_at_most(2000)}
+    it {should validate_length_of(:title).is_at_least(3)} 
+    it {should validate_length_of(:title).is_at_most(250)}
   end
-  it "validate presence to title" do
-    expect(Quest.new(body: '123')).to_not be_valid
-  end
-  
-   it "validate lenght of title min" do
-    expect(Quest.new(title: '13', body: '124r')).to_not be_valid
-  end
-  
-  it "validate lenght of title max" do
-    expect(Quest.new(title: 'f'*251, body: '123')).to_not be_valid
-  end
-  
-   it "validate lenght of body min" do
-    expect(Quest.new(title: '123', body: '12', )).to_not be_valid
-  end
-  
-  it "validate lenght of body max" do
-    expect(Quest.new(title: '123', body: 'f'*2001)).to_not be_valid
-  end
-  
-   it "should have many answers" do
-    t = Quest.reflect_on_association(:answers)
-    expect(t.macro).to eq(:has_many)
-  end
-  
 end
 
 
