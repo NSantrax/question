922389e730642583316fa5ae618f41d5c517f9ca
NSantrax <ns2008p@mail.ru>
2018-07-08 14:32:48 +0300

    Quests controller tests

---
+++ spec/controllers/quests_controller_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+RSpec.describe QuestsController, type: :controller do
+
+  let(:quests) {create_list(:quest, 2)}
+  before { get :index }
+  
+  describe 'GET #index' do
+    it 'populates an array of all questions' do
+      expect(assigns(:quests)).to match_array(quests)
+    end
+    it 'renders index view' do
+      expect(response).to render_template :index
+    end
+  end
+  
+   describe 'GET #show' do
+     let(:quest) {create(:quest)}
+     before {get :show, params: { id: quest }}
+    it 'assigns to requested quest to @quest' do
+      expect(assigns(:quest)).to eq quest
+    end
+    it 'renders show view' do
+      expect(response).to render_template :show
+    end
+  end
+  
+   describe 'GET #new' do
+     
+     before {get :new}
+    it 'assigns a new Quest to @quest' do
+      expect(assigns(:quest)).to be_a_new(Quest)
+    end
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
+  
+  describe 'POST #create' do
+    context 'with valid attribut' do
+      it 'save a new quest in database' do
+        expect {post :create, params: {quest: attributes_for(:quest)}}.to change(Quest, :count).by(1)
+      end
+      it 'redirects to show view' do
+        post :create, params: {quest: attributes_for(:quest)}
+        expect(response).to redirect_to quest_path(assigns(:quest))
+      end
+    end 
+     context 'with invalid attribut' do
+       it 'not save the quest' do
+        expect {post :create, params: {quest: attributes_for(:invalid_quest)}}.to_not change(Quest, :count)
+      end
+      it 're_redirects to show view' do
+        post :create, params: {quest: attributes_for(:invalid_quest)}
+        expect(response).to render_template :new
+      end
+      
+    end
+  end
+end
--- spec/factories/quests.rb
+++ spec/factories/quests.rb
@@ -3,4 +3,9 @@ FactoryBot.define do
     title "MyString"
     body "MyText"
   end
+  
+   factory :invalid_quest, class: "Quest" do
+    title nil
+    body nil
+  end
 end
