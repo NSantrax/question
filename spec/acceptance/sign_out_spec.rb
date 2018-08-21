73839e45d3e4194337d3e4c3da1fb5ed9aaebbd4
NSantrax <ns2008p@mail.ru>
2018-08-21 16:24:24 +0300

    Adding spec sign_up & log out

---
+++ spec/acceptance/sign_out_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+feature 'User sign out', %q{
+  As a log in user
+  I want to be able to log out
+} do
+  given(:user) { create(:user)}
+  scenario 'A logged in user tries to log out' do
+   sign_in(user)
+   click_on 'Выйти' 
+    expect(page).to have_content 'Signed out successfully.'
+    expect(current_path).to eq root_path
+  end
+  scenario 'A non-logged in user tries to log out' do
+  
+   visit root_path
+   expect(page).not_to have_content('Выйти')
+    
+  end
+
+end
---
+++ spec/acceptance/sign_up_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+feature 'User sign up', %q{
+  In order to ask a question
+  as a user
+  I want to be able to sign up
+} do
+  given(:user) { create(:user)}
+  scenario 'A non-registered user tries to sign up' do
+   visit new_user_session_path
+   click_on 'Sign up'
+   fill_in 'Email', with: "usertest@test.com"
+   fill_in 'Password', with: '123456789'
+   fill_in 'Password confirmation', with: '123456789'
+   click_on 'Sign up' 
+    
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+    expect(current_path).to eq root_path
+  end
+  
+  scenario 'A registered user tries to sign up' do
+    visit new_user_session_path  
+    click_on 'Sign up' 
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    fill_in 'Password confirmation', with: user.password
+    click_on 'Sign up'
+    
+    expect(page).to have_content 'Email has already been taken'
+    expect(current_path).to eq "/users"
+  end
+
+end
