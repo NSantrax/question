0a76c5d9b3ba167d528860de1b707376741ba3de
NSantrax <ns2008p@mail.ru>
2018-09-04 00:17:09 +0300

    Adding AJAX

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -7,22 +7,22 @@ RSpec.describe AnswersController, type: :controller do
     user_sign_in
     context 'with valid attribut' do
       it 'save a new answer in database' do
-        expect {post :create, params: {quest_id: quest, answer: attributes_for(:answer)}}.to change(Answer, :count).by(1)
+        expect {post :create, params: {quest_id: quest, answer: attributes_for(:answer)}, format: :js}.to change(Answer, :count).by(1)
       end
       
-      it 'redirects to show view' do
-        post :create, params: {quest_id: quest.id, answer: attributes_for(:answer)}
-        expect(response).to redirect_to quest_path(assigns(:quest))
+      it 'render create template' do
+        post :create, params: {quest_id: quest.id, answer: attributes_for(:answer)}, format: :js
+        expect(response).to render_template 
       end
     end 
      context 'with invalid attribut' do
        it 'not save the quest' do
-        expect {post :create, params: {quest_id: quest, answer: attributes_for(:invalid_answer)}}.to_not change(Answer, :count)
+        expect {post :create, params: {quest_id: quest, answer: attributes_for(:invalid_answer)}, format: :js}.to_not change(Answer, :count)
       end
       
-      it 're_redirects to show view' do
-        post :create, params: {quest_id: quest, answer: attributes_for(:invalid_answer)}
-        expect(response).to redirect_to quest_path(assigns(:quest))
+      it 'render create template' do
+        post :create, params: {quest_id: quest, answer: attributes_for(:invalid_answer)}, format: :js
+        expect(response).to render_template
       end
     end
   end
