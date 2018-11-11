13a47ca569d23af03a093f574b64ee2fa24a4d69
NSantrax <ns2008p@mail.ru>
2018-11-11 21:10:09 +0300

    added comments spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  concern :commentable do
+ concern :commentable do
     resources :comments
   end
-
-  resources :quests, shallow: true, conserns: :commentable do
-    resources :answers, conserns: :commentable
-  end
+  
+  resources :quests, concerns: :commentable, shallow: true do
+      resources :answers, concerns: :commentable
+end
   
   root 'quests#index'
 end
