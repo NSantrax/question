c0de9d8fa4fa04272fe1443b0c7fece2a74543a7
NSantrax <ns2008p@mail.ru>
2018-11-19 21:36:51 +0300

    Authentication with Facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {omniauth_callbacks: 'omniauth_callbacks'}
   
- concern :commentable do
+  concern :commentable do
     resources :comments
   end
   
   resources :quests, concerns: :commentable, shallow: true do
       resources :answers, concerns: :commentable
-end
+  end
   
   root 'quests#index'
 end
---
+++ spec/factories/autorizations.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :autorization do
+    user nil
+    provider "MyString"
+    uid "MyString"
+  end
+end
