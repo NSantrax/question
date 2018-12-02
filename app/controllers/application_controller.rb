b5e1a02effc94c3e10e56cfa0954ce15a9d088a1
NSantrax <ns2008p@mail.ru>
2018-12-02 20:16:02 +0300

    Fix errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ require "application_responder"
 
 class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
-  respond_to :html
+  respond_to :html, :js, :json
   include Pundit
   #protect_form_forgery with: :exeption
 
@@ -16,7 +16,10 @@ class ApplicationController < ActionController::Base
  
     def user_not_authorized
       flash[:warning] = "You are not authorized to perform this action."
-      redirect_to(request.referrer || root_path)
+      respond_to do |format|
+        format.html { redirect_to(request.referrer || root_path) }
+        format.json { render json: @record.errors.full_messages, status: :unprocessable_entity}
+      end
     end
 
 end
