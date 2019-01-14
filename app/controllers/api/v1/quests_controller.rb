b4207dfdcad0134424118cbebd5d24cb92ef6e02
NSantrax <ns2008p@mail.ru>
2019-01-14 21:50:27 +0300

    API for single quest & serializer

--- app/controllers/api/v1/quests_controller.rb
+++ app/controllers/api/v1/quests_controller.rb
@@ -1,9 +1,19 @@
 class Api::V1::QuestsController < Api::V1::BaseController
 
-
+  before_action :load_quest, only: :show
+  
   def index
     @quests = Quest.all
     respond_with @quests
   end
 
+  def show
+    respond_with @quest
+  end
+  
+  private
+
+  def load_quest
+    @quest = Quest.find(params[:id])
+  end
 end
--- app/serializers/quest_serializer.rb
+++ app/serializers/quest_serializer.rb
@@ -1,8 +1,15 @@
 class QuestSerializer < ActiveModel::Serializer
-  attributes :id, :title, :body, :created_at, :updated_at, :short_title
+  attributes :id, :title, :body, :created_at, :updated_at, :short_title, :attach
+
   has_many :answers
+  has_many :comments
+
 
   def short_title
     object.title.truncate(10)
+  end
+
+  def attach
+    object.attachments.map { |a| a.file.url }   
   end
 end
